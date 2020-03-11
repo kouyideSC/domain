14f11acbc320a09ee41bdb6c4e431a5e392bebd7
kouyideSC <kouyichenggong@163.com>
2020-03-11 14:10:50 +0800

    dev新增abc类

---
+++ caipiao-domain-module/src/main/java/com/caipiao/domain/common/Abc.java
@@ -0,0 +1,13 @@
+package com.caipiao.domain.common;
+
+public class Abc {
+    private String a;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
